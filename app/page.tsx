b8160f95fc1e67eafb825f4f623bc4493ff2f177
Rayen khadraoui <rinokhadraoui1@gmail.com>
2026-02-15 00:56:31 +0100

    feat: add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
 import Hero from "@/components/sections/Hero";
 import WhyChooseUs from "@/components/sections/WhyChooseUs";
 import BestSellingProduct from "@/components/sections/BestSellingProduct";
@@ -14,6 +15,7 @@ export default function Home() {
       <BestSellingProduct />
       <ExperienceAndMaterials />
       <Testimonials />
+      <Footer />
     </>
   );
 }
